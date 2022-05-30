a0e79caf61d730dffc372ccfeda1e7a373f8e029
florentunix <florentunix@yahoo.com>
2022-05-30 13:59:53 +0200

    initialisation de la connexion et création de fichier de config et de connexion à la BD

--- database/db.config.ts
+++
@@ -1,6 +0,0 @@
-export const HOST:string = "localhost";
-export const PORT:number = 27017;
-export const USER:string = "etudiant-admin";
-export const PASSWORD:string = "passer2021";
-export const DATABASE:string = "localhost";
-
